c7f40febc2da992b829b3771a4a743518985175d
sibhat <sibhat.temelso@gmail.com>
2019-04-27 15:37:19 -0700

    aws ec2 connection is up and running!

--- tshirt/settings.py
+++ tshirt/settings.py
@@ -80,16 +80,18 @@ REST_FRAMEWORK = {
 WSGI_APPLICATION = 'tshirt.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(default=config("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'clothing',
+        'USER': 'root',
+        'PASSWORD': 'Godislove',
+        'HOST': '34.222.123.230',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True,
+        },
+    }
 }
 
 
@@ -128,7 +130,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 
 STATIC_URL = '/static/'
 import django_heroku
