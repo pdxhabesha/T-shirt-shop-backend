d4c65580806456f21b1e98393ad95ff7cc697764
sibhat <sibhat.temelso@gmail.com>
2019-04-27 17:24:55 -0700

    clean up

--- tshirt/settings.py
+++ tshirt/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import dj_database_url
 from decouple import config
-# import pymysql
-#
-# pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
