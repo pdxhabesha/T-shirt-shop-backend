48cb82b071487bcd1bbc735af19e85468058af8f
sibhat <sibhat.temelso@gmail.com>
2019-04-27 17:24:17 -0700

    view now getting the right db!

--- tshirtapp/views.py
+++ tshirtapp/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tshirtapp.serializers import UserSerializer, GroupSerializer, ProductSerializer
-from tshirtapp.xmodels import Product
+from tshirtapp.models import Product
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -18,6 +18,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
