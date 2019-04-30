e9da78a2cf7252384aa2e46d15f3536e81985e6e
sibhat <sibhat.temelso@gmail.com>
2019-04-30 10:56:12 -0700

    all our serializers are back on now!

--- tshirtapp/serializers.py
+++ tshirtapp/serializers.py
@@ -1,12 +1,11 @@
 from django.db import transaction
 from rest_framework import serializers
-# from .models import Product, Shipping, Tax, Department, Customer, User, Admin
-from .models import Customer, User, Admin
+from .models import Product, Shipping, Tax, Department, Customer, User, Admin
 
 
 class SignupCustomerSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(source='user.email', max_length=255, required=False)
     username = serializers.CharField(source='user.username', max_length=255)
+    email = serializers.EmailField(source='user.email', max_length=255)
     password = serializers.CharField(source='user.password', max_length=128, write_only=True)
     old_password = serializers.CharField(max_length=128, write_only=True, required=False)
     first_name = serializers.CharField(source='user.first_name', allow_blank=True, max_length=30, required=False)
@@ -17,7 +16,6 @@ class SignupCustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['email', 'username',  "first_name", "password", "last_name", "old_password", "is_customer", "is_admin"]
-        # fields = ['email', "password", "username", "old_password", "first_name",  "is_customer", "is_admin"]
 
     @transaction.atomic  # Ensure creation of both models is done
     # in a single transaction not to create inconsistencies
@@ -48,31 +46,31 @@ class SignupAdminSerializer(serializers.ModelSerializer):
         # Create Seeker profile
         return Admin.objects.create(user=user, **validated_data)
 
-#
-# class ProductSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Product
-#         # fields = ("name", "description", "image", "thumbnail", "display")
-#         fields = "__all__"
-#
-#
-# class ShippingSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Shipping
-#         fields = "__all__"
-#
-#
-# class TaxSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Tax
-#         fields = "__all__"
-#
-#
-# class DepartmentSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Department
-#         fields = "__all__"
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        # fields = ("name", "description", "image", "thumbnail", "display")
+        fields = "__all__"
+
+
+class ShippingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Shipping
+        fields = "__all__"
+
+
+class TaxSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tax
+        fields = "__all__"
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Department
+        fields = "__all__"
