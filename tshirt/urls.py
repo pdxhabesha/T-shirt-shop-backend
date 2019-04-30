399e502b8f12e86785e872ceb081c32d631b0d08
sibhat <sibhat.temelso@gmail.com>
2019-04-30 10:52:44 -0700

    urls are now back on

--- tshirt/urls.py
+++ tshirt/urls.py
@@ -19,10 +19,10 @@ from rest_framework import routers
 from tshirtapp import views
 
 router = routers.DefaultRouter()
-# router.register(r'products', views.ProductViewSet)
-# router.register(r'shipping', views.ShippingViewSet)
-# router.register(r'tax', views.TexViewSet)
-# router.register(r'department', views.DepartmentViewSet)
+router.register(r'products', views.ProductViewSet)
+router.register(r'shipping', views.ShippingViewSet)
+router.register(r'tax', views.TexViewSet)
+router.register(r'department', views.DepartmentViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
