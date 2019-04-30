593711d90b1efb17e5133f52cbdf23785e9387f5
sibhat <sibhat.temelso@gmail.com>
2019-04-30 10:55:24 -0700

    base abstract model now added for all user to make use of!

--- tshirtapp/models.py
+++ tshirtapp/models.py
@@ -1,69 +1,42 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-# class UserManager(BaseUserManager):
-#     use_in_migrations = True
-#
-#     def _create_user(self, username, password, **extra_fields):
-#         """
-#         Creates and saves a user with the given username, and password.
-#         """
-#         user = self.model(username=self.normalize_username(username), **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-#
-#     def create_user(self, username, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', False)
-#         extra_fields.setdefault('is_superuser', False)
-#         return self._create_user(username, password, **extra_fields)
-#
-#     def create_superuser(self, username, password, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#
-#         if extra_fields.get('is_staff') is not True:
-#             raise ValueError('Superuser must have is_staff=True.')
-#         if extra_fields.get('is_superuser') is not True:
-#             raise ValueError('Superuser must have is_superuser=True.')
-#
-#         return self._create_user(username, password, **extra_fields)
-#
+class BaseModel(models.Model):
+    """
+    Django abstract model has auto create and update datetime fields.
+    This acts as the base model for all other models.
+    """
+    created_at = models.DateTimeField(verbose_name=_('created time'), auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(verbose_name=_('updated time'), auto_now=True, db_index=True)
 
-class User(AbstractUser):
-    username = models.CharField(max_length=50, unique=True, blank=True)
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, unique=True, blank=True)
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, BaseModel):
     is_customer = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-
-    # objects = UserManager()
-    # USERNAME_FIELD = 'username'
-    # REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
+        unique_together = (('username', 'email'),)
 
     def __str__(self):
-        return f'{self.id}: {self.email} {self.first_name} {self.last_name}'
+        return f'{self.id}: {self.username} {self.first_name} {self.last_name}'
 
 
-class Admin(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+class Admin(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     credit_card = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f'Admin {self.user.first_name}:'
 
-class Customer(models.Model):
+
+class Customer(BaseModel):
     user = models.OneToOneField(verbose_name=_('user'), to='User', related_name='%(class)s', on_delete=models.CASCADE)
     credit_card = models.TextField(blank=True, null=True)
     address_1 = models.CharField(max_length=100, blank=True, null=True)
@@ -78,228 +51,186 @@ class Customer(models.Model):
     mob_phone = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'customer'
 
     def __str__(self):
-        return f'{self.user.first_name}:'
-
-#
-# class DjangoAdminLog(models.Model):
-#     action_time = models.DateTimeField()
-#     object_id = models.TextField(blank=True, null=True)
-#     object_repr = models.CharField(max_length=200)
-#     action_flag = models.SmallIntegerField()
-#     change_message = models.TextField()
-#     content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-#     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'django_admin_log'
-#
-#
-# class DjangoContentType(models.Model):
-#     app_label = models.CharField(max_length=100)
-#     model = models.CharField(max_length=100)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'django_content_type'
-#         unique_together = (('app_label', 'model'),)
-#
-#
-# class DjangoMigrations(models.Model):
-#     app = models.CharField(max_length=255)
-#     name = models.CharField(max_length=255)
-#     applied = models.DateTimeField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'django_migrations'
-#
-#
-# class DjangoSession(models.Model):
-#     session_key = models.CharField(primary_key=True, max_length=40)
-#     session_data = models.TextField()
-#     expire_date = models.DateTimeField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'django_session'
+        return f'Customer {self.user.first_name}:'
 
 
-# class Attribute(models.Model):
-#     attribute_id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=100)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'attribute'
-#
-#
-# class AttributeValue(models.Model):
-#     attribute_value_id = models.AutoField(primary_key=True)
-#     attribute_id = models.IntegerField()
-#     value = models.CharField(max_length=100)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'attribute_value'
-#
-#
-# class Audit(models.Model):
-#     audit_id = models.AutoField(primary_key=True)
-#     order_id = models.IntegerField()
-#     created_on = models.DateTimeField()
-#     message = models.TextField()
-#     code = models.IntegerField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'audit'
-#
-#
-# class Category(models.Model):
-#     category_id = models.AutoField(primary_key=True)
-#     department_id = models.IntegerField()
-#     name = models.CharField(max_length=100)
-#     description = models.CharField(max_length=1000, blank=True, null=True)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'category'
-#
+class Attribute(models.Model):
+    attribute_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
 
-# class Department(models.Model):
-#     department_id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=100)
-#     description = models.CharField(max_length=1000, blank=True, null=True)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'department'
+    class Meta:
+        managed = False
+        db_table = 'attribute'
 
 
-# class OrderDetail(models.Model):
-#     item_id = models.AutoField(primary_key=True)
-#     order_id = models.IntegerField()
-#     product_id = models.IntegerField()
-#     attributes = models.CharField(max_length=1000)
-#     product_name = models.CharField(max_length=100)
-#     quantity = models.IntegerField()
-#     unit_cost = models.DecimalField(max_digits=10, decimal_places=2)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'order_detail'
-#
-#
-# class Orders(models.Model):
-#     order_id = models.AutoField(primary_key=True)
-#     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-#     created_on = models.DateTimeField()
-#     shipped_on = models.DateTimeField(blank=True, null=True)
-#     status = models.IntegerField()
-#     comments = models.CharField(max_length=255, blank=True, null=True)
-#     customer_id = models.IntegerField(blank=True, null=True)
-#     auth_code = models.CharField(max_length=50, blank=True, null=True)
-#     reference = models.CharField(max_length=50, blank=True, null=True)
-#     shipping_id = models.IntegerField(blank=True, null=True)
-#     tax_id = models.IntegerField(blank=True, null=True)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'orders'
-#
-#
-# class Product(models.Model):
-#     product_id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=100)
-#     description = models.CharField(max_length=1000)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     discounted_price = models.DecimalField(max_digits=10, decimal_places=2)
-#     image = models.CharField(max_length=150, blank=True, null=True)
-#     image_2 = models.CharField(max_length=150, blank=True, null=True)
-#     thumbnail = models.CharField(max_length=150, blank=True, null=True)
-#     display = models.SmallIntegerField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'product'
-#
-#
-# class ProductAttribute(models.Model):
-#     product_id = models.IntegerField()
-#     attribute_value_id = models.IntegerField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'product_attribute'
-#         unique_together = (('product_id', 'attribute_value_id'),)
-#
-#
-# class ProductCategory(models.Model):
-#     product_id = models.IntegerField()
-#     category_id = models.IntegerField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'product_category'
-#         unique_together = (('product_id', 'category_id'),)
-#
-#
-# class Review(models.Model):
-#     review_id = models.AutoField(primary_key=True)
-#     customer_id = models.IntegerField()
-#     product_id = models.IntegerField()
-#     review = models.TextField()
-#     rating = models.SmallIntegerField()
-#     created_on = models.DateTimeField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'review'
-#
-#
-# class Shipping(models.Model):
-#     shipping_id = models.AutoField(primary_key=True)
-#     shipping_type = models.CharField(max_length=100)
-#     shipping_cost = models.DecimalField(max_digits=10, decimal_places=2)
-#     shipping_region_id = models.IntegerField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'shipping'
-#
-#
-# class ShippingRegion(models.Model):
-#     shipping_region_id = models.AutoField(primary_key=True)
-#     shipping_region = models.CharField(max_length=100)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'shipping_region'
-#
-#
-# class ShoppingCart(models.Model):
-#     item_id = models.AutoField(primary_key=True)
-#     cart_id = models.CharField(max_length=32)
-#     product_id = models.IntegerField()
-#     attributes = models.CharField(max_length=1000)
-#     quantity = models.IntegerField()
-#     buy_now = models.IntegerField()
-#     added_on = models.DateTimeField()
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'shopping_cart'
-#
-#
-# class Tax(models.Model):
-#     tax_id = models.AutoField(primary_key=True)
-#     tax_type = models.CharField(max_length=100)
-#     tax_percentage = models.DecimalField(max_digits=10, decimal_places=2)
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'tax'
+class AttributeValue(models.Model):
+    attribute_value_id = models.AutoField(primary_key=True)
+    attribute_id = models.IntegerField()
+    value = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'attribute_value'
+
+
+class Audit(models.Model):
+    audit_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField()
+    created_on = models.DateTimeField()
+    message = models.TextField()
+    code = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'audit'
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    department_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+
+
+class Department(models.Model):
+    department_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'department'
+
+
+class OrderDetail(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField()
+    product_id = models.IntegerField()
+    attributes = models.CharField(max_length=1000)
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    unit_cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = 'order_detail'
+
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_on = models.DateTimeField()
+    shipped_on = models.DateTimeField(blank=True, null=True)
+    status = models.IntegerField()
+    comments = models.CharField(max_length=255, blank=True, null=True)
+    customer_id = models.IntegerField(blank=True, null=True)
+    auth_code = models.CharField(max_length=50, blank=True, null=True)
+    reference = models.CharField(max_length=50, blank=True, null=True)
+    shipping_id = models.IntegerField(blank=True, null=True)
+    tax_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'orders'
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.CharField(max_length=150, blank=True, null=True)
+    image_2 = models.CharField(max_length=150, blank=True, null=True)
+    thumbnail = models.CharField(max_length=150, blank=True, null=True)
+    display = models.SmallIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = "product"
+
+
+class ProductAttribute(models.Model):
+    product_id = models.IntegerField(primary_key=True)
+    attribute_value_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = "product_attribute"
+        unique_together = (('product_id', 'attribute_value_id'),)
+
+
+class ProductCategory(models.Model):
+    product_id = models.IntegerField(primary_key=True)
+    category_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = "product_category"
+        unique_together = (('product_id', 'category_id'),)
+
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    customer_id = models.IntegerField()
+    product_id = models.IntegerField()
+    review = models.TextField()
+    rating = models.SmallIntegerField()
+    created_on = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "review"
+
+
+class Shipping(models.Model):
+    shipping_id = models.AutoField(primary_key=True)
+    shipping_type = models.CharField(max_length=100)
+    shipping_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    shipping_region_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = "shipping"
+
+
+class ShippingRegion(models.Model):
+    shipping_region_id = models.AutoField(primary_key=True)
+    shipping_region = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = "shipping_region"
+
+
+class ShoppingCart(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    cart_id = models.CharField(max_length=32)
+    product_id = models.IntegerField()
+    attributes = models.CharField(max_length=1000)
+    quantity = models.IntegerField()
+    buy_now = models.IntegerField()
+    added_on = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "shopping_cart"
+
+
+class Tax(models.Model):
+    tax_id = models.AutoField(primary_key=True)
+    tax_type = models.CharField(max_length=100)
+    tax_percentage = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = "tax"
+
