26a6746df27ebe99bf0ef37390c9f60ace5944fb
sibhat <sibhat.temelso@gmail.com>
2019-05-23 23:24:47 -0700

    redesigning models to represent foreinkey

--- tshirtapp/models.py
+++ tshirtapp/models.py
@@ -38,7 +38,6 @@ class Admin(BaseModel):
 
 class Customer(BaseModel):
     customer_id=models.AutoField(primary_key=True, serialize=False,)
-    user = models.OneToOneField(verbose_name=_('user'), to='User', related_name='%(class)s', on_delete=models.CASCADE)
     credit_card = models.TextField(blank=True, null=True)
     address_1 = models.CharField(max_length=100, blank=True, null=True)
     address_2 = models.CharField(max_length=100, blank=True, null=True)
@@ -46,10 +45,12 @@ class Customer(BaseModel):
     region = models.CharField(max_length=100, blank=True, null=True)
     postal_code = models.CharField(max_length=100, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
-    shipping_region_id = models.IntegerField(blank=True, null=True)
     day_phone = models.CharField(max_length=100, blank=True, null=True)
     eve_phone = models.CharField(max_length=100, blank=True, null=True)
     mob_phone = models.CharField(max_length=100, blank=True, null=True)
+    user = models.OneToOneField(verbose_name=_('user'), to='User', related_name='%(class)s', on_delete=models.CASCADE)
+    shipping_region = models.ForeignKey(verbose_name=_('shipping_region'), to="ShippingRegion",
+                                        on_delete="CASCADE", blank=True, null=True)
 
     class Meta:
         # managed = False
@@ -70,35 +71,19 @@ class Attribute(models.Model):
 
 class AttributeValue(models.Model):
     attribute_value_id = models.AutoField(primary_key=True)
-    attribute_id = models.IntegerField()
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
     value = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
         db_table = 'attribute_value'
 
 
-class Audit(models.Model):
-    audit_id = models.AutoField(primary_key=True)
-    order_id = models.IntegerField()
-    created_on = models.DateTimeField()
-    message = models.TextField()
-    code = models.IntegerField()
+class Cart(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
-        managed = False
-        db_table = 'audit'
-
-
-class Category(models.Model):
-    category_id = models.AutoField(primary_key=True)
-    department_id = models.IntegerField()
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'category'
+        db_table = 'cart'
 
 
 class Department(models.Model):
@@ -107,40 +92,17 @@ class Department(models.Model):
     description = models.CharField(max_length=1000, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'department'
 
 
-class OrderDetail(models.Model):
-    item_id = models.AutoField(primary_key=True)
-    order_id = models.IntegerField()
-    product_id = models.IntegerField()
-    attributes = models.CharField(max_length=1000)
-    product_name = models.CharField(max_length=100)
-    quantity = models.IntegerField()
-    unit_cost = models.DecimalField(max_digits=10, decimal_places=2)
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    department = models.ForeignKey(verbose_name="department_id", to="Department", on_delete=models.CASCADE)
 
     class Meta:
-        managed = False
-        db_table = 'order_detail'
-
-
-class Orders(models.Model):
-    order_id = models.AutoField(primary_key=True)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    created_on = models.DateTimeField()
-    shipped_on = models.DateTimeField(blank=True, null=True)
-    status = models.IntegerField()
-    comments = models.CharField(max_length=255, blank=True, null=True)
-    customer_id = models.IntegerField(blank=True, null=True)
-    auth_code = models.CharField(max_length=50, blank=True, null=True)
-    reference = models.CharField(max_length=50, blank=True, null=True)
-    shipping_id = models.IntegerField(blank=True, null=True)
-    tax_id = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'orders'
+        db_table = 'category'
 
 
 class Product(models.Model):
@@ -153,42 +115,55 @@ class Product(models.Model):
     image_2 = models.CharField(max_length=150, blank=True, null=True)
     thumbnail = models.CharField(max_length=150, blank=True, null=True)
     display = models.SmallIntegerField()
+    product_attribute = models.ManyToManyField(to="AttributeValue")
+    product_category = models.ManyToManyField(to="Category")
 
     class Meta:
-        managed = False
         db_table = "product"
 
 
 class ProductAttribute(models.Model):
-    product_id = models.IntegerField(primary_key=True)
-    attribute_value_id = models.IntegerField()
+    product_id = models.ForeignKey(Product, on_delete="CASCADE")
+    attribute_value = models.ForeignKey(verbose_name="attribute_value_id", to='AttributeValue', on_delete="CASCADE")
 
     class Meta:
-        managed = False
         db_table = "product_attribute"
-        unique_together = (('product_id', 'attribute_value_id'),)
+        # unique_together = (('product_id', 'attribute_value'),)
 
 
 class ProductCategory(models.Model):
-    product_id = models.IntegerField(primary_key=True)
-    category_id = models.IntegerField()
+    product = models.OneToOneField(verbose_name="product_id", to="Product", on_delete="CASCADE")
+    category = models.ForeignKey(verbose_name="category_id", to='AttributeValue', on_delete="CASCADE")
+    name = models.CharField(max_length=100, null=True, blank=True, default="name")
 
     class Meta:
-        managed = False
         db_table = "product_category"
-        unique_together = (('product_id', 'category_id'),)
+        # unique_together = (('product', 'category'),)
+
+
+class OrderDetail(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(to="Orders", on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    attributes = models.CharField(max_length=1000)
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    unit_cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'order_detail'
 
 
 class Review(models.Model):
     review_id = models.AutoField(primary_key=True)
-    customer_id = models.IntegerField()
-    product_id = models.IntegerField()
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     review = models.TextField()
     rating = models.SmallIntegerField()
     created_on = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = "review"
 
 
@@ -196,10 +171,9 @@ class Shipping(models.Model):
     shipping_id = models.AutoField(primary_key=True)
     shipping_type = models.CharField(max_length=100)
     shipping_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    shipping_region_id = models.IntegerField()
+    shipping_region_id = models.ForeignKey(to="ShippingRegion", on_delete=models.CASCADE)
 
     class Meta:
-        managed = False
         db_table = "shipping"
 
 
@@ -208,14 +182,14 @@ class ShippingRegion(models.Model):
     shipping_region = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
         db_table = "shipping_region"
 
 
 class ShoppingCart(models.Model):
     item_id = models.AutoField(primary_key=True)
-    cart_id = models.CharField(max_length=32)
-    product_id = models.IntegerField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
     attributes = models.CharField(max_length=1000)
     quantity = models.IntegerField()
     buy_now = models.IntegerField()
@@ -232,6 +206,32 @@ class Tax(models.Model):
     tax_percentage = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
-        managed = False
         db_table = "tax"
 
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_on = models.DateTimeField()
+    shipped_on = models.DateTimeField(blank=True, null=True)
+    status = models.IntegerField()
+    comments = models.CharField(max_length=255, blank=True, null=True)
+    auth_code = models.CharField(max_length=50, blank=True, null=True)
+    reference = models.CharField(max_length=50, blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    tax = models.ForeignKey(Tax, on_delete=models.CASCADE, null=True)
+    shipping = models.ForeignKey(Shipping, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'orders'
+
+
+class Audit(models.Model):
+    audit_id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Orders, on_delete=models.CASCADE, null=True)
+    created_on = models.DateTimeField()
+    message = models.TextField()
+    code = models.IntegerField()
+
+    class Meta:
+        db_table = 'audit'
